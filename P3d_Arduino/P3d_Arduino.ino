8416b263f0b547bc040495f12a01f9b2e9ac551b
Sebastian Carmona <sebascarm@gmail.com>
2018-04-20 20:55:43 -0300

    Creacion

---
+++ P3d_Arduino/P3d_Arduino.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		P3d_Arduino.ino
+ Created:	20/04/2018 14:39:17
+ Author:	scarmona
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "C_GrupoChip.h"
+#include "C_Comunicacion.h"
+#include "C_Control.h"
+void setup() {
+	 //S
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
